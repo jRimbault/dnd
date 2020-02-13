755f30883bb0beeb95aa8ba5462f09b4a7426300
jRimbault <jacques.rimbault@gmail.com>
2020-02-13 17:13:03 +0100

    wip

--- dnd_rust.py
+++ dnd_rust.py
@@ -6,10 +6,14 @@ import typing
 
 dnd = ctypes.cdll.LoadLibrary(os.path.join("target", "debug", "libdnd.so"))
 dnd.roll_attribute.restype = ctypes.c_uint8
+dnd.roll_attributes.restype = ctypes.POINTER(ctypes.c_uint8)
 
 
 def roll_attributes(dices: int, skipped_dices: int, dice_type: int):
-    yield from (dnd.roll_attribute(dices, skipped_dices, dice_type) for i in range(6))
+    yield from (dnd.roll_attribute(dices, skipped_dices, dice_type) for _ in range(6))
 
 
 print(*roll_attributes(5, 2, 6))
+
+p = dnd.roll_attributes(5, 2, 6)
+print(*[p[i] for i in range(6)])
