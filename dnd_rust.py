d4cda1ed98d595505fe172893e5abd27661d2c55
jRimbault <jacques.rimbault@gmail.com>
2020-02-13 17:08:12 +0100

    init

---
+++ dnd_rust.py
@@ -0,0 +1,15 @@
+import argparse
+import ctypes
+import os
+import random
+import typing
+
+dnd = ctypes.cdll.LoadLibrary(os.path.join("target", "debug", "libdnd.so"))
+dnd.roll_attribute.restype = ctypes.c_uint8
+
+
+def roll_attributes(dices: int, skipped_dices: int, dice_type: int):
+    yield from (dnd.roll_attribute(dices, skipped_dices, dice_type) for i in range(6))
+
+
+print(*roll_attributes(5, 2, 6))
